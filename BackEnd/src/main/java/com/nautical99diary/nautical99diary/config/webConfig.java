ac1ea621357776036eed6f524d8eb0c89616b57c
백현명 <whitewise95@gmail.com>
2022-06-13 11:20:50 +0900

    webConfig 경로 변경

--- BackEnd/src/main/java/com/nautical99diary/nautical99diary/config/webConfig.java
+++ BackEnd/src/main/java/com/nautical99diary/nautical99diary/config/webConfig.java
@@ -1,4 +1,4 @@
-package com.nautical99diary.nautical99diary.temp;
+package com.nautical99diary.nautical99diary.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
